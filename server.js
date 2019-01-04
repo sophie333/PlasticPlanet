c632414a7f9a46733b5d987eac85206f0ff0b5f1
Sophie Amelin <42768125+sophie333@users.noreply.github.com>
2019-01-04 12:25:03 +0100

    server inserts current object in html

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 var http = require("http");
 var url = require("url");
-var ContentArray = require ('./ContentArray.js');
+//var qs = require("querystring");
 var port = 9000;
 
 var server = http.createServer ( function (request , response) {
 	//allow js to accept the reply
 	response.writeHead(200, {"Access-Control-Allow-Origin": "*"});
-	//response.write("<p>URL requested: " + request.url);
 
-	//var contentArray = new ContentArray();
-	//console.log(contentArray);
+	var query = url.parse(request.url, true).query;
+	var title = query.title;
+	var cont = query.cont;
 
-	console.log('a: '+request.method);
-	console.log('b: '+request.headers);
-	console.log('c: '+request.url);
-
-	var urlParts = url.parse(request.url, true);
-
+	response.write('<h1>' + title + '</h1>');
+	response.end('<p>' + cont + '</p>');
+/*
 	if (urlParts.path == "/0") {
 		//let h = contentArray.getContent(0).title;
 		//var p = objArray.getContent(0).content;
@@ -31,11 +28,11 @@ var server = http.createServer ( function (request , response) {
 	else if (urlParts.path == "/2") {
 		response.write('<h1>#3 Dangers of Plastic</h1>');
 		response.end('<p>Some <span style = "color: green;">green </span>text</p>');
-	}
+	}/*
 	else {
 		response.write('<h1>#ERR Not a Danger</h1>');
 		response.end('<p>Some plain text</p>');
-	}
+	}*/
 });
 
 server.listen (port , function () {
